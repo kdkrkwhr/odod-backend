ce5db638431507366479c51fb6ed9ace308ff5c8
KimDongki <kdkdongki1997@gmail.com>
2021-06-04 17:25:28 +0900

    [feat] yml 파일 업데이트 (AWS S3 이용)

--- src/main/java/com/odod/controller/GalleryController.java
+++ src/main/java/com/odod/controller/GalleryController.java
@@ -1,19 +1,28 @@
 package com.odod.controller;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import com.odod.dto.GalleryDto;
 import com.odod.service.GalleryService;
 import com.odod.service.S3Service;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.io.IOException;
 
+@Api(value = "GalleryController")
 @Controller
 @AllArgsConstructor
 public class GalleryController {
+
+  static final Logger logger = LoggerFactory.getLogger(GalleryController.class);
 
   @Autowired
   private S3Service s3Service;
@@ -21,13 +30,14 @@ public class GalleryController {
   @Autowired
   private GalleryService galleryService;
 
-  @GetMapping("/gallery")
+  @RequestMapping(value = "/gallery", method = RequestMethod.GET)
   public String dispWrite() {
 
-    return "/gallery";
+    return "thymeleaf/gallery";
   }
 
-  @PostMapping("/gallery")
+  @ApiOperation(value = "이미지 추가", tags = "이미지 관리")
+  @RequestMapping(value = "/gallery", method = RequestMethod.POST)
   public String execWrite(GalleryDto galleryDto, MultipartFile file) throws IOException {
     String imgPath = s3Service.upload(file);
     galleryDto.setFilePath(imgPath);
